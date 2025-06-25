409b356d8a67d320a13c1a440ea746d4d5650b9b
Piyush Badgujar <piyush.badgujar23@pccoepune.org>
2025-06-26 02:43:56 +0530

    almost done

--- script.js
+++ script.js
@@ -3,74 +3,74 @@ gsap.registerPlugin(ScrollTrigger);
 
 // NAVBAR hide/show on scroll
 let lastScroll = 0;
-const navbar = document.querySelector('.navbar');
-window.addEventListener('scroll', () => {
+const navbar = document.querySelector(".navbar");
+window.addEventListener("scroll", () => {
   const currentScroll = window.scrollY;
   gsap.to(navbar, {
     y: currentScroll > lastScroll && currentScroll > 100 ? -100 : 0,
     duration: 0.5,
-    ease: 'power2.out'
+    ease: "power2.out",
   });
   lastScroll = currentScroll;
 });
 
 // PARALLAX BACKGROUND
-const bg = document.querySelector('.parallax-bg');
-document.querySelector('.home').addEventListener('mousemove', (e) => {
+const bg = document.querySelector(".parallax-bg");
+document.querySelector(".home").addEventListener("mousemove", (e) => {
   const x = (e.clientX / window.innerWidth - 0.5) * 30;
   const y = (e.clientY / window.innerHeight - 0.5) * 30;
   gsap.to(bg, {
-    x, y,
+    x,
+    y,
     rotation: -30,
     duration: 1.2,
-    ease: 'power3.out'
+    ease: "power3.out",
   });
 });
-document.querySelector('.home').addEventListener('mouseleave', () => {
-  gsap.to(bg, { x: 0, y: 0, rotation: -30, duration: 1.2, ease: 'power3.out' });
+document.querySelector(".home").addEventListener("mouseleave", () => {
+  gsap.to(bg, { x: 0, y: 0, rotation: -30, duration: 1.2, ease: "power3.out" });
 });
 
 // ABOUT SECTION Animations
-gsap.from('.about-heading', {
-  x: '-1000',
+gsap.from(".about-heading", {
+  x: "-1000",
   duration: 0.5,
   delay: 0.1,
-  ease: 'power3.out',
+  ease: "power3.out",
   scrollTrigger: {
-    trigger: '.about-heading',
-    start: 'top 80%',
-    toggleActions: 'play none none reverse'
-  }
+    trigger: ".about-heading",
+    start: "top 80%",
+    toggleActions: "play none none reverse",
+  },
 });
 
-gsap.from('.about-left', {
+gsap.from(".about-left", {
   scale: 0.9,
   opacity: 0,
   y: 40,
   duration: 0.5,
-  ease: 'power2.out',
+  ease: "power2.out",
   scrollTrigger: {
-    trigger: '.about-left img',
-    start: 'top 85%',
-    toggleActions: 'play none none reverse'
-  }
+    trigger: ".about-left img",
+    start: "top 85%",
+    toggleActions: "play none none reverse",
+  },
 });
 
-gsap.from('.about-right', {
+gsap.from(".about-right", {
   scale: 0.9,
   opacity: 0,
   y: 40,
   duration: 0.7,
-  ease: 'power2.out',
+  ease: "power2.out",
   scrollTrigger: {
-    trigger: '.about-right',
-    start: 'top 85%',
-    toggleActions: 'play none none reverse'
-  }
+    trigger: ".about-right",
+    start: "top 85%",
+    toggleActions: "play none none reverse",
+  },
 });
-;
 
-// Entrance animation for courses
+// Courses Section Animations
 gsap.from(".courses", {
   y: 100,
   opacity: 0,
@@ -79,10 +79,9 @@ gsap.from(".courses", {
   scrollTrigger: {
     trigger: ".courses",
     start: "top 95%",
-    toggleActions: "play none none reverse"
-  }
+    toggleActions: "play none none reverse",
+  },
 });
-
 gsap.from(".courses h1", {
   y: 80,
   opacity: 0,
@@ -92,12 +91,11 @@ gsap.from(".courses h1", {
   scrollTrigger: {
     trigger: ".courses",
     start: "top 85%",
-    toggleActions: "play none none reverse"
-  }
+    toggleActions: "play none none reverse",
+  },
 });
 
-
-// YOUTUBE VIDEO GALLERY Animations
+// YouTube Gallery Animation
 gsap.from(".youtube-videos h1", {
   opacity: 0,
   y: 50,
@@ -106,10 +104,11 @@ gsap.from(".youtube-videos h1", {
   scrollTrigger: {
     trigger: ".youtube-videos h1",
     start: "top 90%",
-    toggleActions: "play none none reverse"
-  }
+    toggleActions: "play none none reverse",
+  },
 });
 
+// Animate each swiper-slide
 gsap.utils.toArray(".swiper-slide").forEach((slide, index) => {
   gsap.fromTo(
     slide,
@@ -137,8 +136,8 @@ gsap.utils.toArray(".swiper-slide").forEach((slide, index) => {
   );
 });
 
-// Swiper Initialization
-const swiper = new Swiper(".mySwiper", {
+// Swiper for YouTube Gallery or General Carousel
+const mainSwiper = new Swiper(".mySwiper", {
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
@@ -156,6 +155,30 @@ const swiper = new Swiper(".mySwiper", {
     },
     768: {
       slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+// Swiper for Results Section
+const resultsSwiper = new Swiper(".results-swiper", {
+  slidesPerView: 1.2,
+  spaceBetween: 20,
+  loop: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2.5,
     },
     1024: {
       slidesPerView: 3,
